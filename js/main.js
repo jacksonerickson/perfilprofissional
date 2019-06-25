f70a265d46b816b032782878ce166113bd3763ef
JACKSON ERICKSON DA SILVA EMIDIO <jackson.esemidio@senacsp.edu.br>
2019-06-25 17:13:17 -0300

    modificação geral do site

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@
 $(function(){
     var mixer = mixitup('#mix-container');
 });
+
+function Enviar(){
+    var nome = document.getElementById("nomeid");
+    var email = document.getElementById("emailid");
+    var telefone = document.getElementById("telid");
+    var mc = document.getElementById("mcid");
+    if(nome.value && email.value && telefone.value && mc.value != ""){
+        alert('Obrigado' + nome.value + 'entrarei em contato em breve');
+    }
+}
